52f80c8758b8bdd471788f63193fb3a4e94fef1a
Akshat Patel <akshat.patel@globalvoxinc.com>
2022-11-07 17:13:09 +0530

    Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/product");
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -17,6 +22,7 @@ export default function Home() {
           <Link style={{ marginLeft: "10px" }} href="/product">
             Product
           </Link>
+          <button onClick={handleClick}>Place Order</button>
         </nav>
       </header>
       <main className={styles.main}>
