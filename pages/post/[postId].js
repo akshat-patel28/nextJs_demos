d4ed9adccad23a1aacb72aeb0d21592bae9ff23b
Akshat Patel <akshat.patel@globalvoxinc.com>
2022-11-16 11:40:21 +0530

    fallback page for undefined parameter in getStaticPath function

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,9 +1,19 @@
 import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Post({ post }) {
+  // if fallback is true in getStaticPaths function
+  // will get fallback version of page for undefined parameter
+  // page will replaced with new render page after undefined parameter's  data is fetched
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <div>
-      <h1>{post.title}</h1>
+      <h1>
+        {post.id}. {post.title}
+      </h1>
       <p>{post.body}</p>
     </div>
   );
@@ -12,26 +22,26 @@ export default function Post({ post }) {
 // this function will informs posible values for any parmas getStaticProps function
 export async function getStaticPaths() {
   // can write all params staticly
-  //   return {
-  //     paths: [
-  //       {
-  //         params: { postId: "1" },
-  //       },
-  //       {
-  //         params: { postId: "2" },
-  //       },
-  //       {
-  //         params: { postId: "3" },
-  //       },
-  //     ],
-  //     fallback: false,
-  //   };
+  return {
+    paths: [
+      {
+        params: { postId: "1" },
+      },
+      {
+        params: { postId: "2" },
+      },
+      {
+        params: { postId: "3" },
+      },
+    ],
+    fallback: true,
+  };
 
   // or
-  const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
-  const data = res.data;
-  const paths = data.map((post) => ({ params: { postId: `${post.id}` } }));
-  return { paths, fallback: false };
+  // const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+  // const data = res.data;
+  // const paths = data.map((post) => ({ params: { postId: `${post.id}` } }));
+  // return { paths, fallback: true };
 }
 
 export async function getStaticProps(context) {
