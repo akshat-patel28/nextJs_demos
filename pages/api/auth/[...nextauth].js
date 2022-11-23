4fb77af47b1038e02f6a1b0cdeaf3d3688628ff3
Akshat Patel <akshat.patel@globalvoxinc.com>
2022-11-23 15:24:51 +0530

    provider for external login api

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -10,6 +11,28 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
     // ...add more providers here
+    CredentialsProvider({
+      name: "Email",
+      async authorize(credentials, req) {
+        // add the api logic here
+        // if user is authenticated then return jwt
+        const user = {
+          jwt: "udhe9rherhdkfjndiufbiubdfniduf9dhf",
+          name: "xyz",
+        };
+        return user;
+        // else return null
+        // return null
+      },
+      credentials: {
+        username: { label: "Email", type: "email", placeholder: "email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+    }),
   ],
 };
 export default NextAuth(authOptions);
