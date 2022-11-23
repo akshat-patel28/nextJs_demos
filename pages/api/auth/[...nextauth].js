75b709c1f34042452fb8c7876d4e0351cde1198e
Akshat Patel <akshat.patel@globalvoxinc.com>
2022-11-23 15:29:16 +0530

    mock jwt added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -17,8 +17,7 @@ export const authOptions = {
         // add the api logic here
         // if user is authenticated then return jwt
         const user = {
-          jwt: "udhe9rherhdkfjndiufbiubdfniduf9dhf",
-          name: "xyz",
+          jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
         };
         return user;
         // else return null
